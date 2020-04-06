479bdbce9c203d3f1d35accf0c9701026c23c2c1
Zelvax42 <50151743+Zelvax42@users.noreply.github.com>
2020-04-06 16:39:20 -0700

    Agregado despliega_menu()

--- cliente_baraja.py
+++ cliente_baraja.py
@@ -19,15 +19,53 @@ import copy
 import time
 
 
+def despliega_menu():
+    print("--------------------------\n")
+    print("** MENU **\n")
+    print("1.- Pedir mano")
+    print("2.- Mostrar jugadores")
+    print("3.- Mostrar manos de todos")
+    print("4.- Volver a jugar")
+    print("5.- Mostrar marcador")
+    print("0.- Salir")
+    o = input("\nOpción:> ")
+
+    opciones = ['1','2','3','4','5','0']
+    if o in opciones:
+        return int(o)
+    else:
+        print("¡Opción incorrecta!")
+        return despliega_menu()
+
+
 def main(jugador, ip, puerto):
     print("\n== JUEGO DE BARAJA FRANCESA ==\n")
     print("Iniciando...\n")
-    proxy = xmlrpc.client.ServerProxy("http://" + str(ip) + ":" + str(puerto))
 
-    try:
-        print(proxy.prueba_conexion(jugador)) # SOLO USAR PARA TESTING
+    try:        
+        proxy = xmlrpc.client.ServerProxy("http://" + str(ip) + ":" + str(puerto))
+        #print(proxy.prueba_conexion(jugador))  # SOLO USAR PARA TESTING
+        opcion = 10930193413
+        while opcion != 0:
+            opcion = despliega_menu()
+            print("\n")
+            if opcion == 0:
+                pass
+            elif opcion == 1:
+                pass
+            elif opcion == 2:
+                pass
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            elif opcion == 5:
+                pass
+        print("\n¡Gracias por jugar!\n")
     except ConnectionError:
         print("Error de conexión con el servidor.\n")
+    except KeyboardInterrupt:
+        print("Cancelado por el usuario")
 
 
 if __name__ == "__main__":
