836ac377dcc52285eee5a38af08cc9a827b05bf8
Zelvax42 <50151743+Zelvax42@users.noreply.github.com>
2020-04-06 22:11:34 -0700

    Agregada funcion pedir_mano()

--- servidor_baraja.py
+++ servidor_baraja.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python
+'''
+    Equipo: Mustafar
+    Fecha: 10 de abril de 2020
 
-# Equipo: Mustafar
-# Fecha: 10 de abril de 2020
-# Integrantes:
-#
-#   Félix López Juan Pablo
-#   López Velásquez Octavio
-#   Serna Navarro Ángel Emilio
+    Integrantes:
+        Félix López Juan Pablo
+        López Velásquez Octavio
+        Serna Navarro Ángel Emilio
+
+        Server SDK baraja
+        Programa: Servidor.
 '''
-    Server SDK baraja
-    Programa: Servidor.
-'''
+
 import argparse
 from xmlrpc.server import SimpleXMLRPCServer
 import logging
 import random
+import tarjetas
+import pickle
 
 
 def crear_servidor(ip, puerto):
@@ -22,6 +25,51 @@ def crear_servidor(ip, puerto):
     logging.basicConfig(level=logging.DEBUG)
     server = SimpleXMLRPCServer((ip, puerto), logRequests=True,)
     return server
+
+
+def guardar_pickle(baraja, mano):
+    '''
+        guarda una lista de cartas y un objeto baraja en el servidor
+        recibe: un objeto baraja, uso: "baraja"
+        recibe: numero de mano (int) uso: mano
+        guarda en un archivo .pkl la lista
+    '''
+    try:
+        lista = [baraja, mano]
+        pickle.dump(lista, open("pickle.pkl", "wb"))
+        print("Se actualizó", "pickle.pkl")
+    except:
+        print("Hubo un problema al guardar el archivo")
+
+
+def leer_pkl():
+    '''
+        Lee un archivo .pkl y devuelve una lista de objetos
+        regresa: lista[0] = baraja
+                 lista[1] = mano
+    '''
+    try:
+        lista = pickle.load(open("pickle.pkl", "rb"))
+    except:
+        print("No se pudo leer el archivo solicitado. \n"
+              "Intenta hacer una reinserción del mismo.")
+    return lista
+
+
+def pedir_mano(jugador):
+    '''
+        Guarda el jugador en baraja, y te asigna una mano
+        recibe: un nombre de jugador, uso: "emilio"
+        regresa: una lista de cartas
+    '''
+    lista = leer_pkl() # Se utiliza un pickle para poder usar la información del servidor
+    baraja = lista[0]
+    mano = lista[1]
+    tarjetas.genera_jugador(jugador, baraja)
+    lista_cartas = baraja.genera_mano(mano, jugador)
+    print(jugador, "Solicitó pedir mano")
+    guardar_pickle(baraja, mano) # Se guardan los valores sobreescritos
+    return lista_cartas
 
 
 def prueba_conexion(jugador):
@@ -35,7 +83,12 @@ def prueba_conexion(jugador):
 def main(ip, puerto, mano):  # dirección IP, puerto, cantidad de cartas por mano
     server = crear_servidor(ip, puerto)
 
+    lista_cartas = tarjetas.genera_lista_cartas()
+    baraja = tarjetas.Baraja(lista_cartas)
+    guardar_pickle(baraja, mano)
+
     server.register_function(prueba_conexion)
+    server.register_function(pedir_mano)
 
     # Iniciando servidor
     print("\nIniciando servidor...\n")
@@ -48,8 +101,10 @@ def main(ip, puerto, mano):  # dirección IP, puerto, cantidad de cartas por man
         print("\n===========================")
         print("\nUsa Control-C para salir.")
         server.serve_forever()
+
     except KeyboardInterrupt:
-        print("\nSaliendo...\n")
+        print("\nApagando servidor...\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
